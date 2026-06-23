e1d177c3570c0578d404017e3cf4912ff7ae378c
Abdulmajeed Alhasani <aalhasani@neomtech.local>
2026-06-23 09:22:55 +0300

    Initialize Movie Manager project

---
+++ MovieManager/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieManager.Models
+{
+    public class Movie
+    {
+        //تعتبر خصائص وهنا تكتب في المودل
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public decimal Rating { get; set; }
+        
+    }
+}
