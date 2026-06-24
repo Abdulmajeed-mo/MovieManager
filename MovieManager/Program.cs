b3416e8760d30225cb964a8e47d8b268dcc76a6c
Abdulmajeed Alhasani <aalhasani@neomtech.local>
2026-06-24 09:11:41 +0300

    Practice ASP.NET Core routing fundamentals

--- MovieManager/Program.cs
+++ MovieManager/Program.cs
@@ -1,3 +1,4 @@
+//Configuration
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,6 +11,29 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
+
+
+
+
+
+//الماب قت () تكتب داخل البروقرام سي اس  لأنها مسؤولة عن استقبال طلبات القت وربطها بمسار الراوت
+//() => تعني: نفّذ الكود عند استدعاء الـ Route.
+app.MapGet("/movies",()=> "Welcome to Movie Manager");
+//QueryString
+app.MapGet("/search",(string name)=> $"Searching for: {name} ");
+//Route Parameter
+//? يأتي بعد اسم الـ Route Parameter وليس قبله.
+
+//app.MapGet("/movie/{id?}", (int? id )=> id == null ? $"Movie Id not provided" :  $"Movie Id:{id}");
+
+app.MapGet("/movie/{id:int}", (int id) => $"Movie Id:{id}");
+
+
+
+
+
+app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
