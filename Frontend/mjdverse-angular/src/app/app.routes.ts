11ca565b14b268f77ada158d03eceb6041a41826
Abdulmajeed Alhasani <aalhasani@neomtech.local>
2026-09-24 15:07:17 +0300

    Add route guards and custom 404 page

--- Frontend/mjdverse-angular/src/app/app.routes.ts
+++ Frontend/mjdverse-angular/src/app/app.routes.ts
@@ -1,19 +1,19 @@
 import { Routes } from '@angular/router';
 
 import { MainLayout } from './layouts/main-layout/main-layout';
-
 import { Home } from './features/home/home';
 import { Login } from './features/auth/login/login';
 import { Register } from './features/auth/register/register';
 import { VerifyOtp } from './features/auth/verify-otp/verify-otp';
 import { VerifyLoginOtp } from './features/auth/verify-login-otp/verify-login-otp';
+import { NotFound } from './pages/not-found/not-found';
+import { guestGuard } from './core/auth/guards/guest-guard';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayout,
     children: [
-
       // Default route → Home
       {
         path: '',
@@ -30,31 +30,35 @@ export const routes: Routes = [
       // Login page
       {
         path: 'login',
+        canActivate: [guestGuard],
         component: Login
       },
 
       // Register page
       {
         path: 'register',
+        canActivate: [guestGuard],
         component: Register
       },
 
       // Verify registration email OTP
       {
         path: 'verify-otp',
+        canActivate: [guestGuard],
         component: VerifyOtp
       },
 
       // Verify login OTP
       {
         path: 'verify-login-otp',
+        canActivate: [guestGuard],
         component: VerifyLoginOtp
       },
 
-      // Unknown route → Home
+      // 404 - Must be last
       {
         path: '**',
-        redirectTo: 'home'
+        component: NotFound
       }
     ]
   }
